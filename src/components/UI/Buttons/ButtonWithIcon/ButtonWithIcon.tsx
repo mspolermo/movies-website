57b2606cdd1275ae325163face8758dc9c0ccfae
mspolermo <mspolermo@gmail.com>
2023-04-19 16:07:42 +0500

    Footer updated

--- src/components/UI/Buttons/ButtonWithIcon/ButtonWithIcon.tsx
+++ src/components/UI/Buttons/ButtonWithIcon/ButtonWithIcon.tsx
@@ -4,40 +4,36 @@ import classes from './ButtonWithIcon.module.scss'
 
 interface ButtonWithIconProps {
     color?: string;
-    svgLink?: string;
+    svg?: React.ReactElement;
     title?: Array<string>;
-    type?: string;
-    linkTo?: string;
+    onClick?: () => void;
 }
 
-const ButtonWithIcon:FC<ButtonWithIconProps> = ({title, color, svgLink, type, linkTo}) => {
-
+const ButtonWithIcon:FC<ButtonWithIconProps> = ({ color, svg, title, onClick }) => {
     const [colorChanger, setColorChanger] = useState (classes.btn);
 
     useEffect ( () => {
         switch(color) {
             case 'red':
-                setColorChanger (classes.btn + ' ' + classes.btn__red);
+                setColorChanger (`${classes.btn} ${classes.btn__red}`);
                 break;
             case 'gray':
-                setColorChanger (classes.btn + ' ' + classes.btn__gray);
+                setColorChanger (`${classes.btn} ${classes.btn__gray}`);
                 break;
         };
-    },[])
+    },[]);
 
     return (
-        <div className={colorChanger}>
-            {(svgLink) && <img src={svgLink} className={classes.btn__svg}/>}
+        <div className={colorChanger} onClick={onClick}>
+            {(svg) && <div className={classes.btn__svg}>{svg}</div>}
             {(title) && <div className={classes.btn__textBlock}>
                 {title.map(elem => <div 
                     key={elem}
                     className={classes.btn__text}
                 >{elem}</div>)}
             </div>} 
-            
         </div>
-    )
-
-}
+    );
+};
 
 export default ButtonWithIcon;
