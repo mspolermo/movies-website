61f7b353c0924c7fd9d3fad9b6a6f0935c98f965
1lssh <80113936+1lssh@users.noreply.github.com>
2023-04-21 02:58:29 +0300

    base rating range added

--- src/components/RatingRange/RatingRange.tsx
+++ src/components/RatingRange/RatingRange.tsx
@@ -1,10 +1,33 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import './RatingRange.scss'
+
 interface IRatingRangeProps { };
 
-export const RatingRange: FC<IRatingRangeProps> = (props) => {
+export const RatingRange: FC<IRatingRangeProps> = () => {
+	let MAX = 10;
+	const [minValue, setMinValue] = useState<number>(0);
+
+	const handleMinChange = (e: React.FormEvent<HTMLInputElement>) => {
+		setMinValue(Number(e.currentTarget.value))
+	}
+
+	const getBackgroundSize = () => {
+		return { backgroundSize: `${(minValue * 100) / MAX}% 100%` }
+	}
+
 	return (
-		<div>
-			<input type="range" />
+		<div className="input-range">
+			<input
+				className="input-range__item"
+				type="range"
+				value={minValue}
+				min={0}
+				max={10}
+				step={0.1}
+				style={getBackgroundSize()}
+				onChange={handleMinChange}
+			/>
+			<span className="input-range__value">{minValue}</span>
 		</div>
 	);
 }
