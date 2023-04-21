4cc1f8fc0bf5b8b160d2267946f35f81c10aebbd
1lssh <80113936+1lssh@users.noreply.github.com>
2023-04-21 20:11:18 +0300

    Rating range slider reworked

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
-import'./styles/App.scss'
+import './styles/App.scss'
 
-const  App = () => {
-  return (
-    <div>
-      <h1 className='header'>Welcome to Movies-Website!</h1>
-      <Footer />
-    </div>
-  );
+const App = () => {
+	return (
+		<div>
+			<h1 className='header'>Welcome to Movies-Website!</h1>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- src/components/RatingRange/RatingRange.tsx
+++ src/components/RatingRange/RatingRange.tsx
@@ -1,33 +1,60 @@
-import { FC, useState } from "react";
+import { FC, useState, useEffect } from "react";
 import './RatingRange.scss'
 
-interface IRatingRangeProps { };
+interface IRatingRangeProps {
+	filterBy?: string
+};
 
-export const RatingRange: FC<IRatingRangeProps> = () => {
-	let MAX = 10;
+export const RatingRange: FC<IRatingRangeProps> = ({ filterBy = 'rating' }) => {
+
+	useEffect(() => {
+		switch (filterBy) {
+			case 'rating':
+				setMaxValue(10);
+				setStep(0.1);
+				break;
+			case 'rating-count':
+				setMaxValue(100);
+				setStep(10);
+				break;
+		};
+	}, [])
+
 	const [minValue, setMinValue] = useState<number>(0);
+	const [maxValue, setMaxValue] = useState<number>(0);
+	const [step, setStep] = useState<number>(0);
+
+	const newVal = Number(((minValue - 0) * 100) / (maxValue - 0));
 
 	const handleMinChange = (e: React.FormEvent<HTMLInputElement>) => {
 		setMinValue(Number(e.currentTarget.value))
 	}
 
 	const getBackgroundSize = () => {
-		return { backgroundSize: `${(minValue * 100) / MAX}% 100%` }
+		return { backgroundSize: `${(minValue * 100) / maxValue}% 100%` }
 	}
 
 	return (
-		<div className="input-range">
+		<div className="range-slider">
+			<span
+				className="range-slider__bubble"
+				style={{ left: `calc(${newVal}% + (${8 - newVal * 0.13}px))` }}
+			>
+				{minValue}
+			</span>
 			<input
-				className="input-range__item"
+				className="range-slider__input"
 				type="range"
 				value={minValue}
 				min={0}
-				max={10}
-				step={0.1}
-				style={getBackgroundSize()}
+				max={maxValue}
 				onChange={handleMinChange}
+				style={getBackgroundSize()}
+				step={step}
 			/>
-			<span className="input-range__value">{minValue}</span>
+			<div className="range-slider__minmax">
+				<span>0</span><span>10</span>
+			</div>
 		</div>
-	);
+	)
 }
