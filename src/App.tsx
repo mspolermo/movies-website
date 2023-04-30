760c7e4c8a2f825975a672bcf601e1dec01c00ae
Cosmic Woman <mikaat2r1370@gmail.com>
2023-04-30 17:18:45 +0300

    MoviesPage в App

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import Footer from './components/Footer/Footer';
 import'./styles/App.scss'
 import Filters from "./components/Filters/Filters";
 import Header from './components/Header/Header';
+import MoviesPage from "./components/Pages/MoviesPage/MoviesPage";
+import HeaderSearch from "./components/Header/HeaderSearch";
+import FilmCard from "./components/FilmCard/FilmCard";
 
 const  App = () => {
   return (
     <div>
       <Header />
       <div className="container">
-        <Filters/>
+        <MoviesPage/>
+
         <div className="empty">
-          <h1 className='heading'>Welcome to Movies-Website!</h1> 
+          {/*<h1 className='heading'>Welcome to Movies-Website!</h1> */}
         </div>
           
       </div>
