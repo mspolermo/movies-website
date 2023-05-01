6fa2927b449dcb43cec546538a1b2f7d0e99f26d
1lssh <80113936+1lssh@users.noreply.github.com>
2023-05-01 20:29:00 +0300

    Code refactoring

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,6 +1,7 @@
-import { FC, useEffect, useState, useRef, useMemo, Children, useCallback, PropsWithChildren, ReactNode } from "react";
+import { FC, useEffect, useState, useRef } from "react";
 import './Carousel.scss'
 import Icons from "../Icons/Icons";
+import { InfiniteCarousel } from "./InfiniteCarousel/InfiniteCarousel";
 
 interface ICarouselProps {
 	variant: string
@@ -12,107 +13,16 @@ export const Carousel: FC<ICarouselProps> = ({ variant, children }) => {
 	const [scroll, setScroll] = useState<number>(0)
 	const [scrollWidth, setScrollWidth] = useState<any>(0)
 
-	const [current, setCurrent] = useState<number>(1)
-
-	const [translateX, setTranslateX] = useState<number>(0)
-
-	const [sliderItems, setSliderItems] = useState<any>([])
 	const [arrowClassName, setArrowClassName] = useState<string>()
-	const [x1, setX1] = useState<number | null>(null)
-	const [y1, setY1] = useState<number | null>(null)
-
 
 	const el = useRef<any>(null)
-	const slider = useRef<any>(null)
-	const interval = useRef<any>(null)
-	const childWidthRef = useRef<any>(null)
 
 	let block = el.current
 
 
-	const handleTouchStart = (e: any) => {
-		const firstTouch = e.touches[0]
-		setX1(firstTouch.clientX)
-		setY1(firstTouch.clientY)
-	}
-
-	const handleTouchMove = (e: any) => {
-		if (!x1 || !y1) {
-			return false
-		}
-		let x2 = e.touches[0].clientX
-		let y2 = e.touches[0].clientY
-		let xDiff = x2 - x1
-		let yDiff = y2 - y1
-		if (Math.abs(xDiff) > Math.abs(yDiff)) {
-			if (xDiff > 5) {
-				actionHandler('prev')
-			}
-			if (xDiff < -5) {
-				actionHandler('next')
-			}
-		}
-		setX1(null)
-		setY1(null)
-	}
-
-
-	const addActiveClass = (type: string) => {
-		for (let i = 0; i < sliderItems.length; i++) {
-			sliderItems[i].classList.remove("carousel__slider-item_active");
-		}
-		if (sliderItems[current]) {
-
-			if (type === 'prev') {
-				sliderItems[current].classList.add("carousel__slider-item_active")
-				if (current === 1) {
-					sliderItems[current + children.length].classList.add("carousel__slider-item_active")
-				}
-			} else {
-				if (current === children.length) {
-					sliderItems[current - 2].classList.add("carousel__slider-item_active")
-
-				}
-				sliderItems[current + 2].classList.add("carousel__slider-item_active")
-
-
-			}
-		}
-
-	}
-
-	const actionHandler = useCallback((mode: string) => {
-		slider.current.style.transitionDuration = '600ms'
-		if (mode === 'prev') {
-			if (current <= 1) {
-				setTranslateX(0)
-				setCurrent(children.length)
-				addActiveClass('prev')
-			} else {
-				setTranslateX(childWidthRef.current.clientWidth * (current - 1))
-				setCurrent((prev: any) => --prev)
-				addActiveClass('prev')
-			}
-
-		} else if (mode === 'next') {
-			if (current === children.length) {
-				setCurrent(1)
-				setTranslateX(childWidthRef.current.clientWidth * (children.length + 1))
-				addActiveClass('next')
-
-			} else {
-				setTranslateX((childWidthRef.current.clientWidth) * (current + 1))
-				setCurrent((prev: any) => ++prev)
-				addActiveClass('next')
-			}
-		}
-	}, [current, children, sliderItems])
-
 
 	useEffect(() => {
 		let block = el.current
-		setSliderItems(document.querySelectorAll('.carousel__slider-item'))
-		document.querySelectorAll('.carousel__slider-item')[current + 1]?.classList.add('carousel__slider-item_active')
 		switch (variant) {
 			case 'cards':
 				// Ширина прокрутки в процентах от ширины контейнера
@@ -129,55 +39,8 @@ export const Carousel: FC<ICarouselProps> = ({ variant, children }) => {
 				setScrollWidth(500);
 				setArrowClassName('carousel__prev-arrow_icon arrow-sm')
 				break;
-			case 'main':
-				setScrollWidth(block.clientWidth);
-				setArrowClassName('carousel__prev-arrow_icon')
-				setTranslateX(childWidthRef.current.clientWidth)
-				break;
 		}
 	}, [])
-
-	// Для бесконечной прокрутки
-	useEffect(() => {
-		const transitionEnd = () => {
-			if (slider.current) {
-				if (current === 1 && block !== null) {
-					slider.current.style.transitionDuration = '0ms'
-					setTranslateX((childWidthRef.current.clientWidth * current))
-				}
-
-				if (current === children.length) {
-					slider.current.style.transitionDuration = '0ms'
-					setTranslateX(childWidthRef.current.clientWidth * children.length)
-				}
-			}
-
-		}
-		document.addEventListener('transitionend', transitionEnd)
-
-		return () => {
-			document.removeEventListener('transitionend', transitionEnd)
-		}
-
-	}, [current, children])
-
-
-	// Автоматический скролл
-	useEffect(() => {
-		if (slider.current) {
-			if (interval.current) {
-				clearInterval(interval.current)
-			}
-			interval.current = setInterval(() => {
-				actionHandler('next')
-			}, 5000)
-			return () => {
-				if (interval.current) {
-					clearInterval(interval.current)
-				}
-			}
-		}
-	}, [actionHandler])
 
 
 	const btnPressPrev = () => {
@@ -205,42 +68,17 @@ export const Carousel: FC<ICarouselProps> = ({ variant, children }) => {
 	}
 
 
-	const slides: any | Element[] = useMemo(() => {
-		if (children.length > 1) {
-			let items = Children.map(children, (child, index) => (
-				<div ref={childWidthRef} className="carousel__slider-item" key={index}>{child}</div>
-			))
-			return [
-				<div className="carousel__slider-item" key={children.length + 5}>{children[children.length - 2]}</div>,
-				<div className="carousel__slider-item" key={children.length + 1}>{children[children.length - 1]}</div>,
-				...items,
-				<div className="carousel__slider-item" key={children.length + 2}>{children[0]}</div>,
-				<div className="carousel__slider-item" key={children.length + 3}>{children[1]}</div>
-			]
-		}
-
-		return (
-			<div >{children[0]}</div>
-		)
-	}, [children])
-
 	return (
 		<div className="carousel">
-			{scroll > 0 || variant === 'main'
+			{scroll > 0
 				?
 				<div
 					className="carousel__prev-arrow"
-					onClick={variant === 'main' ? () => actionHandler('prev') : btnPressPrev}
-					{...variant === 'main'
-					&&
-					{
-						style: { left: '10%' }
-					}
-					}
+					onClick={btnPressPrev}
 				>
 					<Icons
 						name="arrowLeft"
-						size={variant === 'cards' || variant === 'main' ? '30' : '15'}
+						size={variant === 'cards' ? '30' : '15'}
 						className={arrowClassName}
 						color="rgba(255, 255, 255, 0.7)"
 					/>
@@ -248,21 +86,15 @@ export const Carousel: FC<ICarouselProps> = ({ variant, children }) => {
 				: ''
 			}
 			{
-				block && scroll + block.clientWidth < block.scrollWidth || variant === 'main'
+				block && scroll + block.clientWidth < block.scrollWidth
 					?
 					<div
 						className="carousel__next-arrow"
-						onClick={variant === 'main' ? () => actionHandler('next') : btnPressNext}
-						{...variant === 'main'
-						&&
-						{
-							style: { right: '11%' }
-						}
-						}
+						onClick={btnPressNext}
 					>
 						<Icons
 							name="arrowRight"
-							size={variant === 'cards' || variant === 'main' ? '30' : '15'}
+							size={variant === 'cards' ? '30' : '15'}
 							className={arrowClassName}
 							color="rgba(255, 255, 255, 0.7)"
 						/>
@@ -272,7 +104,7 @@ export const Carousel: FC<ICarouselProps> = ({ variant, children }) => {
 			<div
 				ref={el}
 				className={`
-					${variant === 'main' ? 'carousel__slider-container' : 'carousel__container'} 
+					${'carousel__container'} 
 					${variant === 'tv'
 						&&
 						block
@@ -287,24 +119,17 @@ export const Carousel: FC<ICarouselProps> = ({ variant, children }) => {
 							:
 							'carousel__container_fade-left'
 					}
-					`} >
+					`}
+			>
 				{
 					variant === 'main'
 						?
-						<div
-							className="carousel__slider"
-							onTouchStart={handleTouchStart}
-							onTouchMove={handleTouchMove}
-							ref={slider}
-							style={{ transform: `translate3d(${-translateX}px, 0, 0)` }}
-						>
-							{slides}
-
-						</div>
+						<InfiniteCarousel block={block} children={children} />
 						:
 						children
 				}
 			</div>
+
 		</div>
 	);
 }
---
+++ src/components/Carousel/InfiniteCarousel/InfiniteCarousel.tsx
@@ -0,0 +1,212 @@
+import { Children, FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import Icons from "../../Icons/Icons";
+interface IInfiniteCarouselProps {
+	children: any
+	block: any
+};
+
+export const InfiniteCarousel: FC<IInfiniteCarouselProps> = ({ children, block }) => {
+	const [current, setCurrent] = useState<number>(1)
+
+	const [translateX, setTranslateX] = useState<number>(0)
+
+	const [sliderItems, setSliderItems] = useState<any>([])
+	const [x1, setX1] = useState<number | null>(null)
+	const [y1, setY1] = useState<number | null>(null)
+
+	const el = useRef<any>(null)
+	const slider = useRef<any>(null)
+	const interval = useRef<any>(null)
+	const childWidthRef = useRef<any>(null)
+
+	const handleTouchStart = (e: any) => {
+		const firstTouch = e.touches[0]
+		setX1(firstTouch.clientX)
+		setY1(firstTouch.clientY)
+	}
+
+	const handleTouchMove = (e: any) => {
+		if (!x1 || !y1) {
+			return false
+		}
+		let x2 = e.touches[0].clientX
+		let y2 = e.touches[0].clientY
+		let xDiff = x2 - x1
+		let yDiff = y2 - y1
+		if (Math.abs(xDiff) > Math.abs(yDiff)) {
+			if (xDiff > 5) {
+				actionHandler('prev')
+			}
+			if (xDiff < -5) {
+				actionHandler('next')
+			}
+		}
+		setX1(null)
+		setY1(null)
+	}
+
+
+	const addActiveClass = (type: string) => {
+		for (let i = 0; i < sliderItems.length; i++) {
+			sliderItems[i].classList.remove("carousel__slider-item_active");
+		}
+		if (sliderItems[current]) {
+
+			if (type === 'prev') {
+				sliderItems[current].classList.add("carousel__slider-item_active")
+				if (current === 1) {
+					sliderItems[current + children.length].classList.add("carousel__slider-item_active")
+				}
+			} else {
+				if (current === children.length) {
+					sliderItems[current - 2].classList.add("carousel__slider-item_active")
+
+				}
+				sliderItems[current + 2].classList.add("carousel__slider-item_active")
+
+
+			}
+		}
+
+	}
+
+	const actionHandler = useCallback((mode: string) => {
+		slider.current.style.transitionDuration = '600ms'
+		if (mode === 'prev') {
+			if (current <= 1) {
+				setTranslateX(0)
+				setCurrent(children.length)
+				addActiveClass('prev')
+			} else {
+				setTranslateX(childWidthRef.current.clientWidth * (current - 1))
+				setCurrent((prev: any) => --prev)
+				addActiveClass('prev')
+			}
+
+		} else if (mode === 'next') {
+			if (current === children.length) {
+				setCurrent(1)
+				setTranslateX(childWidthRef.current.clientWidth * (children.length + 1))
+				addActiveClass('next')
+
+			} else {
+				setTranslateX((childWidthRef.current.clientWidth) * (current + 1))
+				setCurrent((prev: any) => ++prev)
+				addActiveClass('next')
+			}
+		}
+	}, [current, children, sliderItems])
+
+	useEffect(() => {
+		setSliderItems(document.querySelectorAll('.carousel__slider-item'))
+		document.querySelectorAll('.carousel__slider-item')[current + 1]?.classList.add('carousel__slider-item_active')
+
+		setTranslateX(childWidthRef.current.clientWidth)
+
+	}, [])
+
+	// Для бесконечной прокрутки
+	useEffect(() => {
+		const transitionEnd = () => {
+			if (slider.current) {
+				if (current === 1 && el.current !== null) {
+					slider.current.style.transitionDuration = '0ms'
+					setTranslateX((childWidthRef.current.clientWidth * current))
+				}
+
+				if (current === children.length) {
+					slider.current.style.transitionDuration = '0ms'
+					setTranslateX(childWidthRef.current.clientWidth * children.length)
+				}
+			}
+
+		}
+		document.addEventListener('transitionend', transitionEnd)
+
+		return () => {
+			document.removeEventListener('transitionend', transitionEnd)
+		}
+
+	}, [current, children])
+
+
+	// Автоматический скролл
+	useEffect(() => {
+		if (slider.current) {
+			if (interval.current) {
+				clearInterval(interval.current)
+			}
+			interval.current = setInterval(() => {
+				actionHandler('next')
+			}, 5000)
+			return () => {
+				if (interval.current) {
+					clearInterval(interval.current)
+				}
+			}
+		}
+	}, [actionHandler])
+
+	const slides: any | Element[] = useMemo(() => {
+		if (children.length > 1) {
+			let items = Children.map(children, (child, index) => (
+				<div ref={childWidthRef} className="carousel__slider-item" key={index}>{child}</div>
+			))
+			return [
+				<div className="carousel__slider-item" key={children.length + 5}>{children[children.length - 2]}</div>,
+				<div className="carousel__slider-item" key={children.length + 1}>{children[children.length - 1]}</div>,
+				...items,
+				<div className="carousel__slider-item" key={children.length + 2}>{children[0]}</div>,
+				<div className="carousel__slider-item" key={children.length + 3}>{children[1]}</div>
+			]
+		}
+
+		return (
+			<div >{children[0]}</div>
+		)
+	}, [children])
+
+
+	return (
+
+		<div
+			ref={el}
+			className='carousel__slider-container'
+		>
+			<div
+				className="carousel__prev-arrow"
+				onClick={() => actionHandler('prev')}
+				style={{ left: '10%' }}
+			>
+				<Icons
+					name="arrowLeft"
+					size={'30'}
+					color="rgba(255, 255, 255, 0.7)"
+					className='carousel__prev-arrow_icon'
+				/>
+			</div>
+			<div
+				className="carousel__next-arrow"
+				onClick={() => actionHandler('next')}
+				style={{ right: '10%' }}
+			>
+				<Icons
+					name="arrowRight"
+					size={'30'}
+					color="rgba(255, 255, 255, 0.7)"
+					className='carousel__prev-arrow_icon'
+				/>
+			</div>
+			<div
+				className="carousel__slider"
+				onTouchStart={handleTouchStart}
+				onTouchMove={handleTouchMove}
+				ref={slider}
+				style={{ transform: `translate3d(${-translateX}px, 0, 0)` }}
+			>
+
+				{slides}
+			</div>
+		</div>
+	);
+}
