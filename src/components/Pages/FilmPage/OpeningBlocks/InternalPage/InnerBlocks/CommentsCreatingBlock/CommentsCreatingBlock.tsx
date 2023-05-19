3ac40706c1aaf85e8a80901a693c6a76ca1a10c3
mspolermo <mspolermo@gmail.com>
2023-05-19 17:43:55 +0500

    FilmPage: CommentCreating completed

--- src/components/Pages/FilmPage/OpeningBlocks/InternalPage/InnerBlocks/CommentsCreatingBlock/CommentsCreatingBlock.tsx
+++ src/components/Pages/FilmPage/OpeningBlocks/InternalPage/InnerBlocks/CommentsCreatingBlock/CommentsCreatingBlock.tsx
@@ -6,6 +6,8 @@ import { useTypedSelector } from "../../../../../../../hooks/useTypedSelector";
 import Button from "../../../../../../UI/Buttons/Button/Button";
 import axios from "axios";
 import Search from "../../../../../../UI/Inputs/Search/Search";
+import Comment from "./Comment/Comment";
+import Review from "./Review/Review";
 
 interface CommentsCreatingBlockProps {
     filmId: number;
@@ -22,9 +24,11 @@ const CommentsCreatingBlock:FC<CommentsCreatingBlockProps> = ({filmId}) => {
     const[sortedComments, setSortedComments] = useState<CommentsStructure | []>([]);
     const [headReview, setHeadReview] = useState('');
     const [bodyReview, setBodyReview] = useState('');
+    const [textComment, setTextComment]  = useState('');
+    const [parentId, setParentId] = useState(0);
 
     useEffect( () => {
-        fetchComments()
+        fetchComments();
     }, [])
 
     async function fetchComments() {
@@ -32,7 +36,19 @@ const CommentsCreatingBlock:FC<CommentsCreatingBlockProps> = ({filmId}) => {
         setSortedComments(response.data.reverse())
     };
 
-    async function createReview() {
+    function openCreateReview (e:React.MouseEvent<HTMLDivElement>) {
+        e.currentTarget.parentElement?.parentElement?.children[1].classList.remove('commentsCreatingBlock__review-form_hidden');
+        e.currentTarget.parentElement?.classList.add('commentsCreatingBlock__btn_hidden');
+    };
+    async function createReview (e:React.MouseEvent<HTMLDivElement>) {
+
+        if ((headReview == '') || (bodyReview == '') ) {
+            alert('Все поля должны быть заполнены');
+            return;
+        }
+
+        e.currentTarget.parentElement?.parentElement?.parentElement?.firstElementChild?.classList.remove('commentsCreatingBlock__btn_hidden');
+        e.currentTarget.parentElement?.parentElement?.classList.add('commentsCreatingBlock__review-form_hidden');
 
         let bodyParameters = {
             "header": headReview,
@@ -46,77 +62,101 @@ const CommentsCreatingBlock:FC<CommentsCreatingBlockProps> = ({filmId}) => {
         }
         );
 
+        await axios.post(`http://localhost:5000/${filmId}`, bodyParameters, {
+            headers: { Authorization: `Bearer ${responseAuth.data.token['token']}` }
+        });
+
+        fetchComments();
+        setHeadReview('');
+        setBodyReview('');
+
+    };
+
+    function openCreateComment (e:React.MouseEvent<HTMLDivElement>) {
+        setParentId(Number(e.currentTarget.parentElement?.lastChild?.textContent));
+        e.currentTarget.parentElement?.parentElement?.parentElement?.parentElement?.children[1].classList.toggle('commentsCreatingBlock__comment-form_hidden');
+    }
+    async function createComment (e:React.MouseEvent<HTMLDivElement>) {
+
+        if (textComment == '') {
+            alert('Все поля должны быть заполнены');
+            return;
+        }
+
+        e.currentTarget.parentElement?.parentElement?.classList.add('commentsCreatingBlock__comment-form_hidden')
+
+        let bodyParameters = {
+            "value": textComment,
+            "parentId": parentId
+        };
+
+        const responseAuth = await axios.post(`http://localhost:5000/login`,
+        {
+            "email": "admin@admin.com",
+            "password": "1234567891011"
+        }
+        );
+
         const sendRewiev = await axios.post(`http://localhost:5000/${filmId}`, bodyParameters, {
             headers: { Authorization: `Bearer ${responseAuth.data.token['token']}` }
         });
         fetchComments();
-    };
 
-    function openCreateReview (e:React.MouseEvent<HTMLDivElement>) {
-        e.currentTarget.parentElement?.children[1].classList.remove('commentsCreatingBlock__form_hidden');
-        e.currentTarget.remove();
-    }
+        setTextComment('')
+
+    };
+    
 
     return (
         <div className="commentsCreatingBlock">
             
             <div className="commentsCreatingBlock__creating">
+
+                <div className="commentsCreatingBlock__btn commentsCreatingBlock__btn_first">
                     <Button title={['Написать отзыв']} color="transparent" type="ultraWide" onClick={openCreateReview}/>
-                <div className="commentsCreatingBlock__form commentsCreatingBlock__form_hidden">
-                    <Search result={[]} renderResult={() => {return []}} placeholder='Введите название отзыва' searchQuery={headReview} setSearchQuery={setHeadReview} cl={false} search={true}/>
-                    <Search result={[]} renderResult={() => {return []}} placeholder='Напишите отзыв' searchQuery={bodyReview} setSearchQuery={setBodyReview} cl={false} search={true}/>
-                    <Button title={['Создать отзыв']} color="transparent" onClick={createReview}/>
+                </div>
+
+                <div className="commentsCreatingBlock__review-form commentsCreatingBlock__review-form_hidden">
+                    
+                    <div className="commentsCreatingBlock__input">
+                        <Search result={[]} renderResult={() => {return []}} placeholder='Название отзыва' searchQuery={headReview} setSearchQuery={setHeadReview} cl={false} search={true}/>
+                    </div>
+
+                    <div className="commentsCreatingBlock__input">
+                        <Search result={[]} renderResult={() => {return []}} placeholder='Текст отзыва' searchQuery={bodyReview} setSearchQuery={setBodyReview} cl={false} search={true}/>
+                    </div>
+
+                    <div className="commentsCreatingBlock__btn">
+                        <Button title={['Создать отзыв']} color="transparent" onClick={createReview}/>
+                    </div>
+                    
                 </div>
             </div>
 
             {(sortedComments.length > 0) && <div className="commentsCreatingBlock__existsComments">
                 
-            {sortedComments.map( (tree) =>
+                {sortedComments.map( (tree) =>
 
                     <div className="commentsCreatingBlock__tree" key={tree[0].id}>
+                        
+                        <Review review={tree[0]} openCreateComment={openCreateComment} />
 
-                        <div className="commentsCreatingBlock__rewiev" >
-                            <div className="commentsCreatingBlock__left">
-                                <Icons name="person" size="60" color="gray" />
-                                <div className="commentsCreatingBlock__text">Chel{tree[0].id}</div>
-                                <div className="commentsCreatingBlock__text">{ (new Date (tree[0].createdAt)).toLocaleDateString() }</div>        
+                        <div className="commentsCreatingBlock__comment-form commentsCreatingBlock__comment-form_hidden" >
+                            
+                            <div className="commentsCreatingBlock__input">
+                                <Search result={[]} renderResult={() => {return []}} placeholder='Текст комментария' searchQuery={textComment} setSearchQuery={setTextComment} cl={false} search={true}/>
                             </div>
-                            <div className="commentsCreatingBlock__right">
-                                <div className="commentsCreatingBlock__top">
-                                    <div className="commentsCreatingBlock__rewiev-header">{tree[0].header}</div>
-                                    <div className="commentsCreatingBlock__rewiev-body">{tree[0].value}</div>
-                                </div>
-                                <div className="commentsCreatingBlock__bottom">
-                                    
-                                    <div className="commentsCreatingBlock__rewiev-like">
-                                        <Icons name="like" size="18" color="gray" />
-                                        <span className="commentsCreatingBlock__likeCounter">{36}</span>
-                                        <Icons name="dislike" size="18" color="gray" />
-                                    </div>
-                                    <Button title={['Комментировать']} color="transparent"/>
-                                </div>
+
+                            <div className="commentsCreatingBlock__btn">
+                                <Button title={['Отправить']} color="transparent" onClick={createComment}/>
                             </div>
+
                         </div>
 
                         { (tree[1].length > 0) && tree[1].map(child => 
-                        
-                        <div className="commentsCreatingBlock__comment" key={child.id} >
-                            <div className="commentsCreatingBlock__comment-left">
-                                <Icons name="person" size="30" color="gray" />
-                                <div className="commentsCreatingBlock__text">Chel{child.id}</div>
-                                <div className="commentsCreatingBlock__text">{ (new Date (child.createdAt)).toLocaleDateString() }</div>  
-                            </div>
-                            <div className="commentsCreatingBlock__comment-right">
-                                <div className="commentsCreatingBlock__comment-body">{child.value}</div>
-                                <div className="commentsCreatingBlock__comment-like">
-                                        <Icons name="like" size="18" color="gray" />
-                                        <span className="commentsCreatingBlock__likeCounter">{ Math.round(tree[0].id + (Math.random()*14*Math.random()*43))}</span>
-                                        <Icons name="dislike" size="18" color="gray" />
-                                    </div>    
-                            </div>
-                        </div>
-                        )
-                        }
+                            <Comment child={child} key={child.authorId}/>
+                        )}
+
                     </div>
                 )}
 
---
+++ src/components/Pages/FilmPage/OpeningBlocks/InternalPage/InnerBlocks/CommentsCreatingBlock/Review/Review.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from "react";
+import './Review.scss'
+import Icons from "../../../../../../../Icons/Icons";
+import Button from "../../../../../../../UI/Buttons/Button/Button";
+import { IComment } from "../../../../../../../../types/filmPageTypes";
+
+interface ReviewProps {
+    review: IComment,
+    openCreateComment: (e:React.MouseEvent<HTMLDivElement>) => void;
+}
+
+const Review:FC <ReviewProps> = ({review, openCreateComment}) => {
+    return (
+        <div className="review" >
+
+            <div className="review__left">
+                <Icons name="person" size="60" color="gray" />
+                <div className="review__text">Chel{review.id}</div>
+                <div className="review__text">{ (new Date (review.createdAt)).toLocaleDateString() }</div>        
+            </div>
+
+            <div className="review__right">
+
+                <div className="review__top">
+                    <div className="review__header">{review.header}</div>
+                    <div className="review__body">{review.value}</div>
+                </div>
+
+                <div className="review__bottom">
+                    
+                    <div className="review__like">
+                        <Icons name="like" size="18" color="gray" className="review__svg"/>
+                        <span className="review__likeCounter">{36}</span>
+                        <Icons name="dislike" size="18" color="gray" className="review__svg"/>
+                    </div>
+                    <Button title={['Комментировать']} color="transparent" onClick={openCreateComment}/>
+                    <div className="review__parentID">{review.id}</div>
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Review;
+
